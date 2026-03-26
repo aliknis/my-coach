ea21708d06547179e2e1e5e82053acbeaa3d24d4
titanknis <titanknis@gmail.com>
2026-03-26 17:17:30 +0100

    fix(auth): change supabase creditentials to production from local creditentials

--- assets/js/supabase_client.js
+++ assets/js/supabase_client.js
@@ -3,9 +3,9 @@ import { createClient } from "https://cdn.jsdelivr.net/npm/@supabase/supabase-js
 // ========================================
 // CONFIGURATION
 // ========================================
-// const SUPABASE_URL = "https://lzexnobseviwzzejnlfr.supabase.co";
-// const SUPABASE_ANON_KEY = "sb_publishable_-Rm_n5vgNqqvjLI7olYvUg_CwX80ug0";
-const SUPABASE_URL = "http://127.0.0.1:54321";
-const SUPABASE_ANON_KEY = "sb_publishable_ACJWlzQHlZjBrEguHvfOxg_3BJgxAaH";
+const SUPABASE_URL = "https://lzexnobseviwzzejnlfr.supabase.co";
+const SUPABASE_ANON_KEY = "sb_publishable_-Rm_n5vgNqqvjLI7olYvUg_CwX80ug0";
+// const SUPABASE_URL = "http://127.0.0.1:54321";
+// const SUPABASE_ANON_KEY = "sb_publishable_ACJWlzQHlZjBrEguHvfOxg_3BJgxAaH";
 
 export const supabase = createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
